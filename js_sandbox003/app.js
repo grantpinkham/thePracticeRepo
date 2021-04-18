f45de257bf6d4c62d77bcc1a637535c4f0809415
grantpinkham <grantpinkham@yahoo.com>
2021-04-18 11:11:34 -0500

    sandboxing

---
+++ js_sandbox003/app.js
@@ -0,0 +1,33 @@
+// primitive
+// string
+const name = 'john doe';
+console.log(typeof name);
+// number
+const age = 32;
+console.log(typeof age);
+// boolean
+const hasKids = true;
+console.log(typeof hasKids);
+// null
+const car = null;
+console.log(typeof car);
+// undefined
+let test;
+console.log(typeof test);
+// symbol
+const sym = Symbol();
+console.log(typeof sym);
+// reference types - objects
+// array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+// object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+console.log(typeof address);
+// date
+const date = new Date();
+console.log(typeof date);
+
