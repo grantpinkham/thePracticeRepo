d02acba0e4a557e503d4ef215bc4b1bd2937b95f
grantpinkham <grantpinkham@yahoo.com>
2021-04-22 09:36:24 -0500

    sandboxing

---
+++ js_sandbox007/app.js
@@ -0,0 +1,27 @@
+const name = 'john';
+const age = 29;
+const job = 'web developer';
+const city = 'miami';
+//without template strings es5
+html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' <li>Job:' +
+        job + ' </li>City: ' + city + ' </li></ul>';
+html = '<ul>' +
+    '<li>Name: ' + name + ' </li>' +
+    '<li>Age: ' + age + ' </li>' +
+    '<li>Job: ' + job + ' </li>' +
+    '<li>City: ' + city + ' </li>' +
+    '</ul>';
+function hello() {
+    return 'hello';
+}
+// template strings/literals es6
+html = `<ul>
+            <li>Name: ${name}</li>
+            <li>Age: ${age}</li>
+            <li>Job: ${job}</li>
+            <li>City: ${city}</li>
+            <li>${5 + 5}</li>
+            <li>${hello()}</li>
+            <li>${age > 29 ? 'over 30' : '29 and below'}</li>
+        </ul>`;
+document.body.innerHTML = html;
