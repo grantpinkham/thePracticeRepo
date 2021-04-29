9cd5820799faaea30551e624dac240514d6a5ea1
grantpinkham <grantpinkham@yahoo.com>
2021-04-29 15:43:14 -0500

    sandboxing

---
+++ js_sandbox014/app.js
@@ -0,0 +1,42 @@
+// // if (something) {
+// //     do something
+// // } else {
+// //     do something else
+// // }
+const id = 100;
+// //equal to
+// if (id == 101) {
+//     console.log('correct');
+// } else {
+//     console.log('incorrect');
+// }
+// //not equal to
+// if (id != 101) {
+//     console.log('correct');
+// } else {
+//     console.log('incorrect');
+// }
+// //equal to value and type
+// if (id === 100) {
+//     console.log('correct');
+// } else {
+//     console.log('incorrect');
+// }
+// //equal to value and type
+// if (id === 100) {
+//     console.log('correct');
+// } else {
+//     console.log('incorrect');
+// }
+//test if undefined
+// if (typeof id !== 'undefined') {
+//     console.log('The ID is ${id}');
+// } else {
+//     console.log('NO ID');
+// }
+//greater or less than
+if (id > 200) {
+    console.log('correct');
+} else {
+    console.log('incorrect');
+}
