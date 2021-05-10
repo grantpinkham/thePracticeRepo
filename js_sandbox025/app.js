b0ac7b6e995784a34f2b2fba10652686b0a7d973
grantpinkham <grantpinkham@yahoo.com>
2021-05-10 16:08:53 -0500

    sandboxing

---
+++ js_sandbox025/app.js
@@ -0,0 +1,27 @@
+//function declarations
+function greet(firstName, lastName) {
+    // if (typeof firstName === 'undefined') {firstName = 'john'}
+    // if (typeof lastName === 'undefined'){lastName = 'doe'}
+    //console.log('hello');
+    return 'hello ' + firstName + ' ' + lastName;
+}
+// console.log(greet());
+//function expressions
+const square = function (x = 3) {
+    return x * x;
+};
+// console.log(square());
+//immediately invokable function expressions - iifes
+// (function () {
+//     console.log('IIFE Ran...');
+// })();
+// (function (name) {
+//     console.log('hello ' + name);
+// })('brad');
+//property methods
+const todo = {
+    add: function() {
+        console.log('add todo...');
+    }
+}
+todo.add();
