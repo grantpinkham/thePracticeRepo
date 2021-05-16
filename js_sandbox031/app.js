2fe78f14933d25b0b1df8e4ebbbeb38805b57a83
grantpinkham <grantpinkham@yahoo.com>
2021-05-16 11:33:22 -0500

    sandboxing

---
+++ js_sandbox031/app.js
@@ -0,0 +1,17 @@
+//for loop
+// for (let i = 0; i < 10; i++) {
+//     if (i == 2) {
+//         console.log('2 is my favorite number');
+//         continue;
+//     }
+//     if (i === 5) {
+//         console.log('stop the loop');
+//         break;
+//     }
+//     console.log('number ' + i);
+// }
+let i = 0;
+while (i < 10) {
+    console.log('number ' + i);
+    i++;
+}
