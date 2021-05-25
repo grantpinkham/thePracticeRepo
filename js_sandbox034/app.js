f20b80287282c699c24d4761b16dd555d1dafb39
grantpinkham <grantpinkham@yahoo.com>
2021-05-25 13:04:52 -0500

    sandboxing

---
+++ js_sandbox034/app.js
@@ -0,0 +1,11 @@
+function isDivisibleBy2or3 () {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 2 == 0) {
+      console.log(`the number above is divisible 2`);
+    } else if (i % 3 == 0) {
+      console.log(`the number above is divisible by 3`);
+    }
+  }
+}
+isDivisibleBy2or3();
