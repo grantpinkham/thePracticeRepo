ba0c864746963c16866a1ceed8895d71c2c1e771
grantpinkham <grantpinkham@yahoo.com>
2021-06-03 15:11:26 -0500

    sandboxing

---
+++ js_sandbox039/app.js
@@ -0,0 +1,21 @@
+const cats = [
+  cat1 = {
+    color: 'orange',
+    age: 4,
+    personality: 'mellow',
+    favoriteFood: 'tuna'
+  },
+  cat2 = {
+    color: 'spotty',
+    age: 6,
+    personality: 'nonchalant',
+    favoriteFood: 'meowmix'
+  },
+  cat3 = {
+    color: 'grey',
+    age: 3,
+    personality: 'fearless',
+    favoriteFood: 'kibble and bits'
+  }
+];
+console.log(cats);
