1ab6ad53318d41b0bb291afd6b474bbf4c3e9dda
grantpinkham <grantpinkham@yahoo.com>
2021-06-04 21:05:12 -0500

    sandboxing

---
+++ js_sandbox040/app.js
@@ -0,0 +1,8 @@
+let catIn = 'I am a cat';
+let aHat = ' in a hat';
+let catInAHat = catIn + aHat;
+if (catInAHat === 'I am a cat in a hat') {
+  console.log('Yes, I am a cat in a hat');
+} else {
+  console.log('No, I am actually a dog');
+}
