40ba4b2bf268c135c0c06e33b7113276d45b94d1
Grant Pinkham <grantpinkham@yahoo.com>
2021-06-06 17:32:54 -0500

    sandboxing

---
+++ java_sandbox009/JavaSandbox009.java
@@ -0,0 +1,26 @@
+public class JavaSandbox009 {
+  public static void main(String[] args) {
+    int count = 0;
+    for (int i = 10; i < 50; i++) {
+      if (isPrime(i)) {
+        count++;
+        System.out.println("number " + i + " is a prime number");
+        if (count == 10) {
+          System.out.println("exiting for loop");
+          break;
+        }
+      }
+    }
+  }
+  public static boolean isPrime(int n) {
+    if (n == 1) {
+      return false;
+    }
+    for (int i = 2; i <= n / 2; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
