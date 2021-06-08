a43b0a71043c0ad8a55cb20651e33beda1514e52
grantpinkham <grantpinkham@yahoo.com>
2021-06-08 10:56:10 -0500

    sandboxing

--- js_sandbox040/app.js
+++ js_sandbox040/app.js
@@ -1,8 +1,7 @@
-let catIn = 'I am a cat';
-let aHat = ' in a hat';
-let catInAHat = catIn + aHat;
-if (catInAHat === 'I am a cat in a hat') {
-  console.log('Yes, I am a cat in a hat');
-} else {
-  console.log('No, I am actually a dog');
+let num = 0;
+for (let x = 0; i < 10; x++) {
+  for (let y = 0; i < 10; y++) {
+    num += x;
+    console.log(y);
+  }
 }
---
+++ js_sandbox041/app.js
@@ -0,0 +1,25 @@
+let count = 0;
+for (let i = 0; i <= 5; i++) {
+  count = i;
+  console.log('number form is ' + count);
+  switch (count) {
+    case 0:
+      console.log('text form is zero');
+      break;
+    case 1:
+      console.log('text form is one');
+      break;
+    case 2:
+      console.log('text form is two');
+      break;
+    case 3:
+      console.log('text form is three');
+      break;
+    case 4:
+      console.log('text form is four');
+      break;
+    case 5:
+      console.log('text form is five');
+      break;
+  }
+}
