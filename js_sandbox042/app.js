c75e4731a78e3e7c76088fadb3463f893661de3c
grantpinkham <grantpinkham@yahoo.com>
2021-06-09 19:20:23 -0500

    sandboxing

---
+++ js_sandbox042/app.js
@@ -0,0 +1,36 @@
+let isVertical = true;
+console.log('this text is horizontal')
+switch (isVertical) {
+  case true:
+    console.log('t');
+  case true:
+    console.log('h');
+  case true:
+    console.log('i');
+  case true:
+    console.log('s');
+  case true:
+    console.log(' ');
+  case true:
+    console.log('i');
+  case true:
+    console.log('s');
+  case true:
+    console.log(' ');
+  case true:
+    console.log('v');
+  case true:
+    console.log('e');
+  case true:
+    console.log('r');
+  case true:
+    console.log('t');
+  case true:
+    console.log('i');
+  case true:
+    console.log('c');
+  case true:
+    console.log('a');
+  case true:
+    console.log('l');
+}
