efaa47d776f70512220c1a0b809efc1db3596168
grantpinkham <grantpinkham@yahoo.com>
2021-06-11 18:19:46 -0500

    sandboxing

---
+++ js_sandbox044/app.js
@@ -0,0 +1,9 @@
+for (let a = 0; a < 2; a++) {
+  for (let b = 0; b < 2; b++) {
+    for (let c = 0; c < 2; c++) {
+      if (a <= 1 && b <= 1 && c <= 1) {
+        console.log(a + ' ' + b + ' ' + c);
+      }
+    }
+  }
+}
