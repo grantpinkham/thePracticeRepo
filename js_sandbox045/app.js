ec092f8ce674f73fafe81c2bc84a9c8552818df7
grantpinkham <grantpinkham@yahoo.com>
2021-06-12 19:02:03 -0500

    sandboxing

---
+++ js_sandbox045/app.js
@@ -0,0 +1,19 @@
+for (let a = 0; a < 2; a++) {
+  for (let b = 0; b < 2; b++) {
+    for (let c = 0; c < 2; c++) {
+      for (let d = 0; d < 2; d++) {
+        for (let e = 0; e < 2; e++) {
+          for (let f = 0; f < 2; f++) {
+            for (let g = 0; g < 2; g++) {
+              for (let h = 0; h < 2; h++) {
+                if (a <= 1 && b <= 1 && c <= 1) {
+                  console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g + ' ' + h);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
