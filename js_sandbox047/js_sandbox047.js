53bf3a376ee39f15f6d10dabc37fb6c0ff290f2d
Grant Pinkham <grantpinkham@yahoo.com>
2021-06-17 19:13:20 -0500

    sandboxing

---
+++ js_sandbox047/js_sandbox047.js
@@ -0,0 +1,9 @@
+console.log('above the number 10 and below the number 100, these are the numbers divisible by both 2 and 3');
+for (let i = 2; i <= 100; i++) {
+  while (i <= 6) {
+    i += i;
+  }
+  if ((i % 2 == 0) && (i % 3 == 0)) {
+    console.log(`${i} is divisible by 2 and 3`);
+  }
+}
