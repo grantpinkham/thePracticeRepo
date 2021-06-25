9970049441771be864a6f846ce2178237fb34f90
grantpinkham <grantpinkham@yahoo.com>
2021-06-25 18:42:42 -0400

    sandboxing

---
+++ js_sandbox053/app.js
@@ -0,0 +1,7 @@
+function Leopard(name, hasSpots, numSpots) {
+  this.name = name;
+  this.hasSpots = hasSpots;
+  this.numSpots = numSpots;
+}
+const leopold = new Leopard('Leopold', true, 4);
+console.log(leopold);
