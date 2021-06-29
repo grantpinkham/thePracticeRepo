1177094d5b43dd395d35a05e7091fc9532bdac44
grantpinkham <grantpinkham@yahoo.com>
2021-06-29 17:39:00 -0400

    sandboxing

---
+++ js_sandbox056/app.js
@@ -0,0 +1,11 @@
+let letters = 'abcdef';
+for (let i = 0; i <= letters.length - 1; i++) {
+  console.log(letters[i]);
+  if (letters[i] === letters[5]) {
+    console.log('g');
+    console.log('h');
+    console.log('i');
+    console.log('just');
+    console.log('kidding');
+  }
+}
