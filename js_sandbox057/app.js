d4e55234f6c65117f5a1535729884acd799d97e7
grantpinkham <grantpinkham@yahoo.com>
2021-06-30 20:03:40 -0400

    sandboxing

---
+++ js_sandbox057/app.js
@@ -0,0 +1,16 @@
+let time = 0;
+let infinity;
+let responsibilities = 0;
+function addTime(num) {
+  return +10;
+}
+function decreaseTime(num) {
+  responsibilities = 10;
+  return -10;
+}
+decreaseTime();
+if (responsibilities === 0) {
+  console.log('must be nice');
+} else {
+  console.log(decreaseTime());
+}
