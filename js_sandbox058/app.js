4815a23474209f6e042726fef66e55a46ea78551
grantpinkham <grantpinkham@yahoo.com>
2021-07-07 15:15:20 -0400

    sandboxing

---
+++ js_sandbox058/app.js
@@ -0,0 +1,10 @@
+let countdown = 10;
+console.log('commencing space launch in...');
+for (let i = 10; i <= 10; i--) {
+  countdown = i;
+  console.log(countdown);
+  if (countdown === 1) {
+    console.log('lift off!');
+    break;
+  }
+}
