6ee20cb27ba3529007df94d34a346eff078cba7d
grantpinkham <grantpinkham@yahoo.com>
2021-07-10 16:52:43 -0400

    sandboxing

---
+++ js_sandbox061/app.js
@@ -0,0 +1,15 @@
+let exercise = function(minutes) {
+  if (minutes < 60 && minutes >= 1) {
+    console.log('exercise has begun, your goal is 60 minutes');
+  } else if (minutes <= 120 & minutes >= 60) {
+    console.log('1 hour elapsed, be sure to hydrate');
+  } else if (minutes > 120) {
+    console.log('good job');
+  } else {
+    console.log('1 minute until exercise begins');
+  }
+}
+exercise(0);
+exercise(1);
+exercise(60);
+exercise(121);
