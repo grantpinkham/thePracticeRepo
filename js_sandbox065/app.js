1ed4c61eef51cc53d9fe9c5243d0e1773418c09f
grantpinkham <grantpinkham@yahoo.com>
2021-07-14 07:50:19 -0400

    sandboxing

---
+++ js_sandbox065/app.js
@@ -0,0 +1,12 @@
+const iniMeeny = [['ini'], ['meeny'], ['miny'], ['moe'], [[['tag! your it!']]]];
+const startIniMeeny = () => {
+  const iniMeenyLength = iniMeeny.length;
+  const newIniMeeny = iniMeeny.flat(50);
+  for (let i = 0; i < iniMeenyLength; i++) {
+    console.log(newIniMeeny[i]);
+    if (iniMeeny[0] === iniMeeny[i] || iniMeeny[1] === iniMeeny[i] || iniMeeny[2] === iniMeeny[i] || iniMeeny[3] === iniMeeny[i]) {
+      console.log('***i hope its not me***');
+    }
+  }
+}
+startIniMeeny();
