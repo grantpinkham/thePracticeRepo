86b2c9a942360b7964eb2196d9fc621b0254fbbc
grantpinkham <grantpinkham@yahoo.com>
2021-07-16 19:01:54 -0400

    sandboxing

---
+++ js_sandbox067/app.js
@@ -0,0 +1,16 @@
+const jsArray = ['j',
+                 'a',
+                 'v',
+                 'a',
+                 's',
+                 'c',
+                 'r',
+                 'i',
+                 'p',
+                 't'
+];
+const jsLength = jsArray.length;
+for (let i = 0; i < 1; i++) {
+  const newArray = jsArray.join('');
+  console.log(newArray);
+}
