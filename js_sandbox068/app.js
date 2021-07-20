61aaa63ff4fc73eaf844243c78ca2f5a59e2b07c
grantpinkham <grantpinkham@yahoo.com>
2021-07-20 03:54:39 -0400

    sandboxing

---
+++ js_sandbox068/app.js
@@ -0,0 +1,12 @@
+const timeOfDay = ['morning', 'afternoon', 'night'];
+const userInput = timeOfDay[0];
+const determineTimeOfDay = () => {
+  if (userInput === timeOfDay[0]) {
+    console.log('i need coffee')
+  } else if (userInput === timeOfDay[1]) {
+    console.log('i need a break')
+  } else {
+    console.log('i need sleep')
+  }
+}
+determineTimeOfDay();
