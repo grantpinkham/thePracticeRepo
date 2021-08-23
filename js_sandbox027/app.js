dae98bff574a00227e024e13e96fb974d44125f8
Grant Pinkham <grantpinkham@yahoo.com>
2021-08-23 13:32:23 -0500

    sandboxing

--- js_sandbox027/app.js
+++ js_sandbox027/app.js
@@ -6,7 +6,6 @@ function greet(firstName, lastName) {
 const square = function (x = 3) {
     return x * x;
 };
-// console.log(square());
 //immediately invokable function expressions - iifes
 // (function () {
 //     console.log('IIFE Ran...');
