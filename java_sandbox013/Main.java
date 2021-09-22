9d2952ebc1a5efa40742d114df68b5f1ad803d20
Grant Pinkham <grantpinkham@yahoo.com>
2021-09-22 17:15:54 -0500

    sandboxing

--- java_sandbox013/Main.java
+++ java_sandbox013/Main.java
@@ -1,7 +1,7 @@
 package java_sandbox013;
 public class Main {
     public static void main(String[] args) {
-        Toy toy = new Toy("red");
+        Toy toy = new Toy("blue");
         Toy anotherToy = toy;
         System.out.println(toy.getColor());
         System.out.println(anotherToy.getColor());
