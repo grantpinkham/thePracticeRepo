c5efbbc7454bfe9d76c6bdc1bcc1a7b1acd7ac5b
grantpinkham <grantpinkham@yahoo.com>
2021-09-23 14:39:39 -0500

    sandboxing

---
+++ js_sandbox072/app.js
@@ -0,0 +1,27 @@
+const dueDate = new Date();
+const absoluteDueDate = dueDate.getDay() + 3;
+switch (absoluteDueDate) {
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+    console.log("monday");
+    break;
+  case 3:
+    console.log("tuesday");
+    break;
+  case 4:
+    console.log("wednesday");
+    break;
+  case 5:
+    console.log("thursday");
+    break;
+  case 6:
+    console.log("friday");
+    break;
+  case 7:
+    console.log("saturday");
+    break;
+  default:
+    console.log("unknown");
+}
