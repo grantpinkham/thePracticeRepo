5ebbf0e37cc44765be89ad4cb882a6f165486566
Grant Pinkham <grantpinkham@yahoo.com>
2021-10-23 08:45:53 -0500

    sandboxing

--- js_sandbox037/app.js
+++ js_sandbox037/app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
-const heading = document.querySelector('h4');
+const heading = document.querySelector('h3');
 taskInput.value = '';
 //keydown
 taskInput.addEventListener('keydown', runEvent);
