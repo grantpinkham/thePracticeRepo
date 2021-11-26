d93944b7e74af26e84e9bbb7a128c9484045cce5
Grant Pinkham <grantpinkham@yahoo.com>
2021-11-26 11:45:03 -0600

    sandboxing

--- js_sandbox023/app.js
+++ js_sandbox023/app.js
@@ -3,7 +3,7 @@ function greet(firstName, lastName) {
     return 'Hello ' + firstName + ' ' + lastName;
 }
 //function expressions
-const square = function (x = 2) {
+const square = function (x = 3) {
     return x * x;
 };
 console.log(square());
