c608ff10c6f89b49fb5d371b99ac928467994bb9
Grant Pinkham <grantpinkham@yahoo.com>
2022-01-21 13:45:06 -0600

    sandboxing

--- js_sandbox022/app.js
+++ js_sandbox022/app.js
@@ -2,7 +2,7 @@
 function greet(firstName, lastName) {
     if (typeof firstName === 'undefined') {firstName = 'john'}
     if (typeof lastName === 'undefined'){lastName = 'doe'}
-    return 'hello ' + firstName + ' ' + lastName;
+    return 'Hello ' + firstName + ' ' + lastName;
 }
 console.log(greet());
 console.log(greet('steve', 'smith'));
