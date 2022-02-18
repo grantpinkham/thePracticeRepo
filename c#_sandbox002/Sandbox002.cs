583085e16cb4f371698594e6e20ef8ac2989ceda
grantpinkham <grantpinkham@yahoo.com>
2022-02-18 09:04:13 -0600

    sandboxing

---
+++ c#_sandbox002/Sandbox002.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Sandbox002
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduce()
+        {
+            Console.WriteLine("Hi my name is " + Name);
+        }
+    }
+}
