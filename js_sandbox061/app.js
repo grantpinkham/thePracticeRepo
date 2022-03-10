32cacfca5411cd885bfb36d3c147cf4fc827c37d
Grant Pinkham <grantpinkham@yahoo.com>
2022-03-10 17:44:48 -0600

    sandboxing

--- js_sandbox061/app.js
+++ js_sandbox061/app.js
@@ -2,7 +2,7 @@ let exercise = function(minutes) {
   if (minutes < 60 && minutes >= 1) {
     console.log('exercise has begun, your goal is 60 minutes');
   } else if (minutes <= 120 & minutes >= 60) {
-    console.log('1 hour elapsed, be sure to hydrate');
+    console.log('1 hour has elapsed, be sure to hydrate');
   } else if (minutes > 120) {
     console.log('good job');
   } else {
