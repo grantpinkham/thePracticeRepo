26f59a29b2d08fd65906eb636a2c351bacf1288f
grantpinkham <grantpinkham@yahoo.com>
2022-03-30 10:21:04 -0500

    sandboxing

---
+++ c#_sandbox003/Sandbox003.cs
@@ -0,0 +1,15 @@
+namespace Sandbox003
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double disc;
+            string customerStatus = "Silver";
+            if (customerStatus == "Gold")
+            {
+                disc = .1;
+            }
+        }
+    }
+}
