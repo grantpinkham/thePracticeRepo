03a4c507c9bc7c8f3e8d20ebec0e13ac6b44360f
Grant Pinkham <grantpinkham@yahoo.com>
2022-04-11 01:54:48 -0500

    Delete java_sandbox003 directory

--- java_sandbox003/JavaSandbox003.java
+++
@@ -1,7 +0,0 @@
-public class JavaSandbox003 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
-    }
-}
