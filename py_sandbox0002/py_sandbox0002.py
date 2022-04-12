467719345d8f6ef6e2b37ff8ef181f1a9063920d
Grant Pinkham <grantpinkham@yahoo.com>
2022-04-12 07:02:52 -0500

    Delete py_sandbox0002 directory

--- py_sandbox0002/py_sandbox0002.py
+++
@@ -1,4 +0,0 @@
-movie_title = '\'The Princess Bride\''
-source_quote = f'My favorite quote from the {movie_title} is ... \n'
-inigo_montoya = '\'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.\''
-print(source_quote + inigo_montoya)
