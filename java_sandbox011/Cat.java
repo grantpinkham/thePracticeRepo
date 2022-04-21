43c3b08787c7c6557b140a17300ba3ac3f3e3f65
Grant Pinkham <grantpinkham@yahoo.com>
2022-04-21 07:11:17 -0500

    Delete java_sandbox011 directory

--- java_sandbox011/Cat.java
+++
@@ -1,8 +0,0 @@
-package java_sandbox011;
-public class Cat extends Animal {
-    private int teeth;
-    private int claws;
-    public Cat(int eyes, int body, int legs, int ears, int teeth, int claws) {
-        super(eyes, body, legs, ears);
-    }
-}
