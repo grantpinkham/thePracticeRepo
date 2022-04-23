babbd0b3c2a3e8fee65c819b9643b14ff9dca2e0
Grant Pinkham <grantpinkham@yahoo.com>
2022-04-23 01:58:53 -0500

    Delete java_sandbox013 directory

--- java_sandbox013/Main.java
+++
@@ -1,9 +0,0 @@
-package java_sandbox013;
-public class Main {
-    public static void main(String[] args) {
-        Toy toy = new Toy("blue");
-        Toy anotherToy = toy;
-        System.out.println(toy.getColor());
-        System.out.println(anotherToy.getColor());
-    }
-}
