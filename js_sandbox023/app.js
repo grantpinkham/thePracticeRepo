4b8379a0dd0f2404c47219d01a5d285b57040ba6
Grant Pinkham <grantpinkham@yahoo.com>
2022-05-08 01:09:19 -0500

    Delete js_sandbox023 directory

--- js_sandbox023/app.js
+++
@@ -1,9 +0,0 @@
-//function declarations
-function greet(firstName, lastName) {
-    return 'Hello ' + firstName + ' ' + lastName;
-}
-//function expressions
-const square = function (x = 3) {
-    return x * x;
-};
-console.log(square());
