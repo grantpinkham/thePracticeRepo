7a6d21dd50b995a537f6f77ce3e29483a8f75b61
Grant Pinkham <grantpinkham@yahoo.com>
2022-05-16 01:17:08 -0500

    Delete js_sandbox053 directory

--- js_sandbox053/app.js
+++
@@ -1,7 +0,0 @@
-function Leopard(name, hasSpots, numSpots) {
-  this.name = name;
-  this.hasSpots = hasSpots;
-  this.numSpots = numSpots;
-}
-const leopold = new Leopard('Leopold', true, 4);
-console.log(leopold);
