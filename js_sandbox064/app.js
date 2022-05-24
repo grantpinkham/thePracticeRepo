9b97455c566cbab9db36158cd22ff267334c881c
Grant Pinkham <grantpinkham@yahoo.com>
2022-05-24 03:37:58 -0500

    sandboxing

--- js_sandbox064/app.js
+++ js_sandbox064/app.js
@@ -2,7 +2,7 @@ let windowVariableCannotReachScopeFunction = true;
 let windowVariable = true;
 while (windowVariable) {
   let scopeFn = () => {
-    console.log(`inside this arrow function, and inside this while loop, it\'s ${windowVariable} that it can access windowVariable`);
+    console.log(`Inside this arrow function, and inside this while loop, it\'s ${windowVariable} that it can access windowVariable.`);
   }
   scopeFunction();
   break;
