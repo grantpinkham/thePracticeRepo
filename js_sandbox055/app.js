c0cf57eed0b6028aaf7f138493df257a120b2119
Grant Pinkham <grantpinkham@yahoo.com>
2022-05-26 04:44:41 -0500

    sandboxing

--- js_sandbox055/app.js
+++ js_sandbox055/app.js
@@ -1,9 +1,9 @@
 for (let i = 0; i <= 10; i++) {
   if (i === 5) {
-    console.log(`first for loop has reached ${i} which is halfway to 10`);
+    console.log(`First for loop has reached ${i} which is halfway to 10.`);
     for (let i = 6; i <= 10; i++) {
       if (i === 10) {
-        console.log(`second for loop has reached ${i} which has completed the loop`);
+        console.log(`Second for loop has reached ${i} which has completed the loop.`);
         break;
       }
     }
