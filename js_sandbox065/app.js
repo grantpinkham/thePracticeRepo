3a8289a2c93bbc80ac18838d338b008af8d5d222
Grant Pinkham <grantpinkham@yahoo.com>
2022-05-28 08:38:57 -0500

    sandboxing

--- js_sandbox065/app.js
+++ js_sandbox065/app.js
@@ -1,4 +1,4 @@
-const iniMeeny = [['ini'], ['meeny'], ['miny'], ['moe'], [[['tag! your it!']]]];
+const iniMeeny = [['Ini'], ['Meeny'], ['Miny'], ['Moe'], [[['Tag! your it!']]]];
 const startIniMeeny = () => {
   const iniMeenyLength = iniMeeny.length;
   const newIniMeeny = iniMeeny.flat(50);
