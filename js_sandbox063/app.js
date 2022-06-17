05a4c24f4f822df033ef347b57a76e094dbb6f02
Grant Pinkham <grantpinkham@yahoo.com>
2022-06-17 00:57:57 -0500

    sandboxing

--- js_sandbox063/app.js
+++ js_sandbox063/app.js
@@ -9,9 +9,9 @@ function playGame() {
   const decreaseScore = function (decrease) {
     return score -= 10;
   }
-  console.log(`your score is ${score}`);
-  console.log(`you increased your score by ${increaseScore()}`);
-  console.log(`you made a big win, your score is now ${scoreMultiplier(10, 4)}`);
-  console.log(`you made a foul, score decreased by 10`);
+  console.log(`Your score is ${score}.`);
+  console.log(`You increased your score by ${increaseScore()}.`);
+  console.log(`You made a big win, your score is now ${scoreMultiplier(10, 4)}.`);
+  console.log(`You made a foul, score decreased by 10.`);
 }
 playGame();
