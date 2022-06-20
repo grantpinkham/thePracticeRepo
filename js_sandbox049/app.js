17bf55d9471ed7bbd580eb095d2bc9a516b64088
Grant Pinkham <grantpinkham@yahoo.com>
2022-06-20 12:38:49 -0500

    sandboxing

--- js_sandbox049/app.js
+++ js_sandbox049/app.js
@@ -2,9 +2,9 @@ function add(x, y) {
   return x + y;
 }
 if (add(2, 2) === 4) {
-  console.log('but you already knew what 2 + 2 was . . .')
+  console.log('But you already knew what 2 + 2 was . . .')
 } else if (add(2, 2) != 4) {
-  console.log('that would require the programmer to change the value him/herself');
+  console.log('That would require the programmer to change the value him/herself.');
 } else {
-  console.log('this will never be executed, until the programmer changes the code in which it is executed...');
+  console.log('This will never be executed, until the programmer changes the code in which it is executed...');
 }
