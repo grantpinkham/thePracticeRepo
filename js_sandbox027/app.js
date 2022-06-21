7cf8eb88e9691354ecf46860aec1230cb8ac8396
Grant Pinkham <grantpinkham@yahoo.com>
2022-06-21 07:18:56 -0500

    sandboxing

--- js_sandbox027/app.js
+++ js_sandbox027/app.js
@@ -9,14 +9,14 @@ const square = function (num = 2) {
 //property methods
 const todo = {
     add: function() {
-        console.log('add todo...');
+        console.log('Add todo...');
     },
     edit: function (id) {
-        console.log(`edit todo ${id}`);
+        console.log(`Edit todo ${id}`);
     }
 }
 todo.delete = function(){
-    console.log('delete todo...');
+    console.log('Delete todo...');
 }
 todo.add();
 todo.edit(22);
