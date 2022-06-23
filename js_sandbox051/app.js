5e07a52b765de4f5e61d11b881f089ca4221fbe8
Grant Pinkham <grantpinkham@yahoo.com>
2022-06-23 08:56:13 -0500

    sandboxing

--- js_sandbox051/app.js
+++ js_sandbox051/app.js
@@ -1,6 +1,6 @@
 let numCompare = 0;
 let numMax = 0;
-let numArray = [1, 5, 29, 100, 22, 4, 99, 200, 2];
+let numArray = [1, 4, 29, 100, 22, 4, 99, 200, 2];
 for (let i = 0; i < numArray.length; i++) {
   if (numArray[i] > numCompare) {
     numCompare = numArray[i];
