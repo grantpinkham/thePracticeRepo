5eac50ed7066b483e8ee5ad4f52a891725b69dd5
Grant Pinkham <grantpinkham@yahoo.com>
2022-07-13 19:49:17 -0500

    sandboxing

--- js_sandbox039/app.js
+++ js_sandbox039/app.js
@@ -1,21 +1,21 @@
 const cats = [
   cat1 = {
-    color: 'Black',
+    color: 'Calico',
     age: 4,
     personality: 'Mellow',
     favoriteFood: 'Tuna'
   },
   cat2 = {
-    color: 'Spotty',
+    color: 'Cinnamon',
     age: 6,
     personality: 'Nonchalant',
-    favoriteFood: 'Meowmix'
+    favoriteFood: 'Meow Mix'
   },
   cat3 = {
     color: 'Grey',
     age: 3,
     personality: 'Fearless',
-    favoriteFood: 'Kibbles and Bits'
+    favoriteFood: 'Fancy Feast'
   }
 ];
 console.log(cats);
